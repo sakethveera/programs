328cb3ffcbfa9380632d84f570e2cbad7c9e4298
Saketh Veera Narayana <95211477+sakethveera@users.noreply.github.com>
2023-09-04 21:50:10 +0530

    Update HOME WORK.cpp

--- HOME WORK.cpp
+++ HOME WORK.cpp
@@ -1,14 +1,45 @@
-#include<stdio.h>
-  main()
-     {
-       int i=42;
-	   printf(" i value is %d , ++i\n");
-	    i=42;
-	   printf(" i value is %d , i++\n");
-	    i=42;
-	   printf(" i value is %d , --i\n");
-	    i=42;
-	   printf(" i value is %d , i--\n");		
-    
-	 }
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
+app = Flask(__name__)
+
+# Database setup
+def init_db():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+    conn.commit()
+    conn.close()
+    
+    return redirect(url_for('success'))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)XA
